4f6e6b69433aae2b830ee7cf77196362443442d4
Aleksandras Zagorskas <aleksandras.zagorskas@engman-arccon.com>
2020-07-29 10:53:05 +0300

    cleanup

--- RevitTest.Schedules/ExternalCommands/ExportScheduleDataCommand.cs
+++ RevitTest.Schedules/ExternalCommands/ExportScheduleDataCommand.cs
@@ -47,27 +47,11 @@ namespace RevitTest.Schedules.ExternalCommands
                     ElementId scheduleId = scheduleInstance.ScheduleId;
                     ViewSchedule schedule = doc.GetElement(scheduleId) as ViewSchedule;
 
-                    TableData tableData = schedule.GetTableData();
-                    TableSectionData tableBodySection = tableData.GetSectionData(SectionType.Body);
-                    int numOfRows = tableBodySection.NumberOfRows;
-                    int numOfCols = tableBodySection.NumberOfColumns;
-                    var cellData = new string[numOfRows, numOfCols];
-
-                    for (int i = 0; i < numOfRows; i++)
-                    {
-                        string rowLine = "";
-                        for (int j = 0; j < numOfCols; j++)
-                        {
-                            cellData[i, j] = tableBodySection.GetCellText(i, j);
-                            rowLine += $"{cellData[i, j]}   ";
-                        }
-                        Console.WriteLine(rowLine);
-                    }
-
                     string exportFileName = Path.Combine(settings.EXCEL_EXPORT_FOLDER, $"{schedule.Name}.csv");
 
                     ExportSchedule(schedule, exportFileName);
 
+                    #region test-data
                     var rng = new Random();
                     var rngs = new string[] { "apple", "orange", "kiwi", "L", "W", "air", "metal" };
 
@@ -84,8 +68,9 @@ namespace RevitTest.Schedules.ExternalCommands
                         testData2.Add(new TestData { Val1 = $"{rngs[rng.Next(0, rngs.Length)]} {i}", Val2 = $"{rngs[rng.Next(0, rngs.Length)]} {i}", Val3 = $"{rngs[rng.Next(0, rngs.Length)]} {i}" });
                     }
                     CSVUtilities.ExportList(settings.EXCEL_TEST_DATA_2_PATH, testData2);
+                    #endregion
 
-                    CreateExcelSpreadsheet(new string[] { exportFileName, settings.EXCEL_TEST_DATA_PATH, settings.EXCEL_TEST_DATA_2_PATH });
+                    CreateExcelSpreadsheet(settings.EXCEL_TEMPLATE_PATH, new string[] { exportFileName, settings.EXCEL_TEST_DATA_PATH, settings.EXCEL_TEST_DATA_2_PATH });
                     //TestCreateExcelSpreadsheet();
                 }
                 else
@@ -113,234 +98,29 @@ namespace RevitTest.Schedules.ExternalCommands
             schedule.Export(Path.GetDirectoryName(exportFileName), Path.GetFileName(exportFileName), exportOptions);
         }
 
-        #region example
-        private void TestCreateExcelSpreadsheet()
+        private void CreateExcelSpreadsheet(string templatePath, string[] dataFilePaths)
         {
-            Excel.Application oXL;
-            Excel._Workbook oWB;
-            Excel._Worksheet oSheet;
-            Excel.Range oRng;
+            Excel.Application excelApp;
+            Excel.Workbook workbook;
+            Excel.Worksheet sheet;
 
             try
             {
                 //Start Excel and get Application object.
-                oXL = new Excel.Application();
-                oXL.Visible = true;
+                excelApp = new Excel.Application();
+                excelApp.Visible = false;
 
                 //Get a new workbook.
-                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
-                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+                workbook = excelApp.Workbooks.Open(templatePath);
+                sheet = (Excel.Worksheet)workbook.ActiveSheet;
 
-                //Add table headers going cell by cell.
-                oSheet.Cells[1, 1] = "First Name";
-                oSheet.Cells[1, 2] = "Last Name";
-                oSheet.Cells[1, 3] = "Full Name";
-                oSheet.Cells[1, 4] = "Salary";
-
-                //Format A1:D1 as bold, vertical alignment = center.
-                oSheet.get_Range("A1", "D1").Font.Bold = true;
-                oSheet.get_Range("A1", "D1").VerticalAlignment =
-                Excel.XlVAlign.xlVAlignCenter;
-
-                // Create an array to multiple values at once.
-                string[,] saNames = new string[5, 2];
-
-                saNames[0, 0] = "John";
-                saNames[0, 1] = "Smith";
-                saNames[1, 0] = "Tom";
-                saNames[1, 1] = "Brown";
-                saNames[2, 0] = "Sue";
-                saNames[2, 1] = "Thomas";
-                saNames[3, 0] = "Jane";
-                saNames[3, 1] = "Jones";
-                saNames[4, 0] = "Adam";
-                saNames[4, 1] = "Johnson";
-
-                //Fill A2:B6 with an array of values (First and Last Names).
-                oSheet.get_Range("A2", "B6").Value2 = saNames;
-
-                //Fill C2:C6 with a relative formula (=A2 & " " & B2).
-                oRng = oSheet.get_Range("C2", "C6");
-                oRng.Formula = "=A2 & \" \" & B2";
-
-                //Fill D2:D6 with a formula(=RAND()*100000) and apply format.
-                oRng = oSheet.get_Range("D2", "D6");
-                oRng.Formula = "=RAND()*100000";
-                oRng.NumberFormat = "$0.00";
-
-                //AutoFit columns A:D.
-                oRng = oSheet.get_Range("A1", "D1");
-                oRng.EntireColumn.AutoFit();
-
-                //Manipulate a variable number of columns for Quarterly Sales Data.
-                DisplayQuarterlySales(oSheet);
+                var schedulePage = new SchedulePage(sheet, "A1", "K17", dataFilePaths);
+                schedulePage.CreatePages();
 
                 //Make sure Excel is visible and give the user control
                 //of Microsoft Excel's lifetime.
-                oXL.Visible = true;
-                oXL.UserControl = true;
-            }
-            catch (Exception theException)
-            {
-                String errorMessage;
-                errorMessage = "Error: ";
-                errorMessage = String.Concat(errorMessage, theException.Message);
-                errorMessage = String.Concat(errorMessage, " Line: ");
-                errorMessage = String.Concat(errorMessage, theException.Source);
-
-                MessageBox.Show(errorMessage, "Error");
-            }
-        }
-
-        private void DisplayQuarterlySales(Excel._Worksheet oWS)
-        {
-            Excel._Workbook oWB;
-            Excel.Series oSeries;
-            Excel.Range oResizeRange;
-            Excel._Chart oChart;
-            String sMsg;
-            int iNumQtrs;
-
-            //Determine how many quarters to display data for.
-            for (iNumQtrs = 4; iNumQtrs >= 2; iNumQtrs--)
-            {
-                sMsg = "Enter sales data for ";
-                sMsg = String.Concat(sMsg, iNumQtrs);
-                sMsg = String.Concat(sMsg, " quarter(s)?");
-
-                DialogResult iRet = MessageBox.Show(sMsg, "Quarterly Sales?",
-                MessageBoxButtons.YesNo);
-                if (iRet == DialogResult.Yes)
-                    break;
-            }
-
-            sMsg = "Displaying data for ";
-            sMsg = String.Concat(sMsg, iNumQtrs);
-            sMsg = String.Concat(sMsg, " quarter(s).");
-
-            MessageBox.Show(sMsg, "Quarterly Sales");
-
-            //Starting at E1, fill headers for the number of columns selected.
-            oResizeRange = oWS.get_Range("E1", "E1").get_Resize(Missing.Value, iNumQtrs);
-            oResizeRange.Formula = "=\"Q\" & COLUMN()-4 & CHAR(10) & \"Sales\"";
-
-            //Change the Orientation and WrapText properties for the headers.
-            oResizeRange.Orientation = 38;
-            oResizeRange.WrapText = true;
-
-            //Fill the interior color of the headers.
-            oResizeRange.Interior.ColorIndex = 36;
-
-            //Fill the columns with a formula and apply a number format.
-            oResizeRange = oWS.get_Range("E2", "E6").get_Resize(Missing.Value, iNumQtrs);
-            oResizeRange.Formula = "=RAND()*100";
-            oResizeRange.NumberFormat = "$0.00";
-
-            //Apply borders to the Sales data and headers.
-            oResizeRange = oWS.get_Range("E1", "E6").get_Resize(Missing.Value, iNumQtrs);
-            oResizeRange.Borders.Weight = Excel.XlBorderWeight.xlThin;
-
-            //Add a Totals formula for the sales data and apply a border.
-            oResizeRange = oWS.get_Range("E8", "E8").get_Resize(Missing.Value, iNumQtrs);
-            oResizeRange.Formula = "=SUM(E2:E6)";
-            oResizeRange.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).LineStyle
-            = Excel.XlLineStyle.xlDouble;
-            oResizeRange.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).Weight
-            = Excel.XlBorderWeight.xlThick;
-
-            //Add a Chart for the selected data.
-            oWB = (Excel._Workbook)oWS.Parent;
-            oChart = (Excel._Chart)oWB.Charts.Add(Missing.Value, Missing.Value,
-            Missing.Value, Missing.Value);
-
-            //Use the ChartWizard to create a new chart from the selected data.
-            oResizeRange = oWS.get_Range("E2:E6", Missing.Value).get_Resize(
-            Missing.Value, iNumQtrs);
-            oChart.ChartWizard(oResizeRange, Excel.XlChartType.xl3DColumn, Missing.Value,
-            Excel.XlRowCol.xlColumns, Missing.Value, Missing.Value, Missing.Value,
-            Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-            oSeries = (Excel.Series)oChart.SeriesCollection(1);
-            oSeries.XValues = oWS.get_Range("A2", "A6");
-            for (int iRet = 1; iRet <= iNumQtrs; iRet++)
-            {
-                oSeries = (Excel.Series)oChart.SeriesCollection(iRet);
-                String seriesName;
-                seriesName = "=\"Q";
-                seriesName = String.Concat(seriesName, iRet);
-                seriesName = String.Concat(seriesName, "\"");
-                oSeries.Name = seriesName;
-            }
-
-            oChart.Location(Excel.XlChartLocation.xlLocationAsObject, oWS.Name);
-
-            //Move the chart so as not to cover your data.
-            oResizeRange = (Excel.Range)oWS.Rows.get_Item(10, Missing.Value);
-            oWS.Shapes.Item("Chart 1").Top = (float)(double)oResizeRange.Top;
-            oResizeRange = (Excel.Range)oWS.Columns.get_Item(2, Missing.Value);
-            oWS.Shapes.Item("Chart 1").Left = (float)(double)oResizeRange.Left;
-        }
-        #endregion
-
-        private void CreateExcelSpreadsheet(string[] paths/*, ViewSchedule schedule*/)
-        {
-
-
-            //var lines = File.ReadAllLines(fileName);
-
-            ////Number of rows including header
-            //var numOfRows = lines.Count();
-
-            ////Number of rows columns
-            //var numOfCols = lines[0].Split(',').Count();
-
-            Excel.Application oXL;
-            Excel.Workbook oWB;
-            Excel.Worksheet oSheet;
-
-            try
-            {
-                //Start Excel and get Application object.
-                oXL = new Excel.Application();
-                oXL.Visible = false;
-
-                //Get a new workbook.
-                oWB = oXL.Workbooks.Open(settings.EXCEL_TEMPLATE_PATH);
-                oSheet = (Excel.Worksheet)oWB.ActiveSheet;
-
-                //Excel.PageSetup pageSetup = oSheet.PageSetup;
-                //var leftMargin = pageSetup.LeftMargin;
-                //var rightMargin = pageSetup.RightMargin;
-                //var topMargin = pageSetup.TopMargin;
-                //var botMargin = pageSetup.BottomMargin;
-
-                //Excel.Range topRange = oSheet.Cells[leftMargin, rightMargin];
-                //Excel.Range sideRange = oSheet.Cells[topMargin, botMargin];
-                //Excel.Range testRange = oSheet.Range[topRange, sideRange];
-                //testRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Salmon);
-
-                Excel.Range pageRange = oSheet.Range["A1", "K17"];
-                //pageRange.Copy();
-
-                //Excel.Range startCellRange = pageRange.EntireColumn.Find("{first-cell}", Missing.Value, Excel.XlFindLookIn.xlValues, Missing.Value, Missing.Value, Excel.XlSearchDirection.xlNext, false, false, Missing.Value);
-                //startCellRange.Value2 = "";
-                //Excel.Range endCellRange = pageRange.EntireColumn.Find("{last-cell}", Missing.Value, Excel.XlFindLookIn.xlValues, Missing.Value, Missing.Value, Excel.XlSearchDirection.xlNext, false, false, Missing.Value);
-                //endCellRange.Value2 = "";
-
-                //Excel.Range bodyRange = oSheet.Range[startCellRange, endCellRange];
-
-                var test = new SchedulePage(oSheet, "A1", "K17", paths);
-                test.CreatePages();
-
-                //int itemsPerPage = bodyRange.Rows.Count;
-                ////int pageCount = numOfRows / itemsPerPage;
-                //int pageCount = (numOfRows + itemsPerPage - 1) / itemsPerPage;
-
-
-
-                //Make sure Excel is visible and give the user control
-                //of Microsoft Excel's lifetime.
-                oXL.Visible = true;
-                oXL.UserControl = true;
+                excelApp.Visible = true;
+                excelApp.UserControl = true;
             }
             catch (Exception ex)
             {
@@ -520,11 +300,6 @@ namespace RevitTest.Schedules.ExternalCommands
             copyRange.Clear();
         }
 
-        //public int GetPageCount()
-        //{
-        //    return (numOfRows + itemsPerPage - 1) / itemsPerPage;
-        //}
-
         public void CreatePages()
         {
             //populate clipboard
@@ -541,7 +316,6 @@ namespace RevitTest.Schedules.ExternalCommands
                 //populate table data
                 PopulateTableData(table.Value);
             }
-
         }
 
         private void PopulateTableData(RevitTableData table)
